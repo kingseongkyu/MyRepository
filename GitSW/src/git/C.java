759883ec109ca69c52f438a992034d00d09b47a5
seong <seong@hanseongkyu>
2025-12-05 21:58:28 +0900

    master modify C

--- GitSW/src/git/C.java
+++ GitSW/src/git/C.java
@@ -4,7 +4,7 @@ public class C {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("C-Project");
+		System.out.println("C-Project in master");
 	}
 
 }
